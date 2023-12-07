eace96867989cf28ffde339685582e1633c56ca8
liangshaobo2001 <77974322+liangshaobo2001@users.noreply.github.com>
2023-12-06 21:09:08 -0500

    Updated Run_M2.1
    Updated Run_M2.1.ipynb and .py so that it takes the M1 output.

--- Run_M2.1.py
+++ Run_M2.1.py
@@ -1,38 +1,40 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-
-
 import torch
 from transformers import AutoTokenizer
 from transformers import AutoModelForSequenceClassification
 from transformers import get_scheduler
 from torch.utils.data import Dataset, DataLoader, SubsetRandomSampler, random_split
+from transformers import DistilBertTokenizerFast
 import evaluate as evaluate
 from tqdm import tqdm
 import matplotlib
 import matplotlib.pyplot as plt
 import copy
 import json
-from transformers import DistilBertTokenizerFast
 
 
 class ArticleDataset(Dataset):
     def __init__(self, json_file):
-        with open(json_file, 'r') as input_file:
+        with open(json_file, 'r', encoding='utf-8') as input_file:
             self.data = json.load(input_file)
+        self.articles = []
+        self.articles.append(self.data['match']['docs'][0]['body'][0])
+        for doc in self.data['response']['docs']:
+            self.articles.append(doc['body'][0])
         self.tokenizer = DistilBertTokenizerFast.from_pretrained('distilbert-base-uncased')
 
     def __len__(self):
-        return 1
+        return len(self.articles)
     def __getitem__(self, idx):
-        x_token = self.tokenizer(self.data,
+        x_token = self.tokenizer(self.articles[idx],
                                  padding='max_length',
                                  max_length=512,
                                  truncation=True,
                                  return_tensors='pt')        
         return {'id':x_token['input_ids'][0], 'attention_mask':x_token['attention_mask'][0]}
-    
+
 def evaluate_model(model, dataloader, device, acc_only=True):
     """ Evaluate a PyTorch Model
     :param torch.nn.Module model: the model to be evaluated
@@ -59,56 +61,39 @@ def evaluate_model(model, dataloader, device, acc_only=True):
         attention_mask = batch['attention_mask'].to(device)
         output = model(input_ids=input_ids, attention_mask=attention_mask)
         
-    
+       
         predictions = output.logits
         predictions = torch.argmax(predictions, dim=1)
         label.append(predictions)
-
+    
     return label
 
-def label_to_hyperpartisan(label):
-    if label == 0:
-        return 'false'
-    elif label == 1:
-        return 'true'
-    
-def label_to_leaning(label):
-    if label == 0:
-        return 'left'
-    elif label == 1:
-        return 'right'
-    elif label == 2:
-        return 'center'
-    elif label == 3:
-        return 'undefined'
+def get_political_perspective(leaning, hyperpartisan):
+    return (leaning-1) * (hyperpartisan+1)
 
 def main():
-
     device = 'cuda'
-
+    batch_size = 1
+    train_mask = range(50)
+    model_name = 'distilbert-base-uncased'
     leaning_model_dir = './saved_models/leaning/'
     hyperpartisan_model_dir = './saved_models/hyperpartisan/'
+    dataset = ArticleDataset(json_file='./M1_output.json')
+    dataloader = DataLoader(dataset, shuffle=False, batch_size = 1)
 
-
-    dataset = ArticleDataset(json_file='./input/input_article.json')
-    dataloader = DataLoader(dataset)
-
-
-    # leaning_model = AutoModelForSequenceClassification.from_pretrained(leaning_model_dir)
     hyperpartisan_model = AutoModelForSequenceClassification.from_pretrained(hyperpartisan_model_dir)
     hyperpartisan_model.to(device)
     leaning_model = AutoModelForSequenceClassification.from_pretrained(leaning_model_dir)
     leaning_model.to(device)
 
-    leaning_prediction = evaluate_model(leaning_model, dataloader, device)
-    hyperpartisan_prediction = evaluate_model(hyperpartisan_model, dataloader, device)
+    leaning_predictions = evaluate_model(leaning_model, dataloader, device)
+    hyperpartisan_predictions = evaluate_model(hyperpartisan_model, dataloader, device)
 
-    print(f"Political Leaning: {label_to_leaning(int(leaning_prediction[0].data))}")
-    print(f"Is Hyperpartisan: {label_to_hyperpartisan(int(hyperpartisan_prediction[0].data))}")
+    coarse_perspectives = []
+    for i in range(len(leaning_predictions)):
+        coarse_perspectives.append(int(get_political_perspective(leaning_predictions[i], hyperpartisan_predictions[i]).data))
+
+    print(coarse_perspectives)
 
 if __name__ == "__main__":
     main()
-
-
-
-
