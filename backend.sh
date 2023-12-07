0f33efd3842995b9c5ccb2c2d077188ba9b75580
Haowei An <anhaowei@gateway-01.dmz>
2023-12-07 00:23:01 -0500

    add backend

---
+++ backend.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Step 1: Get input ID
+read -p "Enter the input ID: " input_id
+
+# Step 2: Run Solr command (Replace 'your_solr_command' with the actual Solr command)
+echo "Running Solr command..."
+your_solr_command
+
+# Step 3: Run a Python file (Replace 'your_python_script1.py' with the actual Python script)
+echo "Running Python script 1..."
+python your_python_script1.py
+
+# Step 4: Run another Python file (Replace 'your_python_script2.py' with the actual Python script)
+echo "Running Python script 2..."
+python your_python_script2.py
+
+# End of the script
+echo "Script execution completed."
